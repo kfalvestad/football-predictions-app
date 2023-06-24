6c569fdf0e93f4b57432b1be6d7fb22fde963339
elefantoft <KjetilAlvestad@gmail.com>
2023-06-24 13:16:07 +0200

    Update gameweek router getAll query to not include fixtures

--- src/server/api/routers/gameweek.ts
+++ src/server/api/routers/gameweek.ts
@@ -1,20 +1,8 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { type RouterOutputs } from "~/utils/api";
-
-export type GammeweekWithFixtures = RouterOutputs["gameweek"]["getAll"][number];
 
 export const gameweekRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
-    const gameweeks = await ctx.prisma.gameweek.findMany({
-      include: {
-        fixtures: {
-          include: {
-            homeTeam: true,
-            awayTeam: true,
-          },
-        },
-      },
-    });
+    const gameweeks = await ctx.prisma.gameweek.findMany({});
 
     return gameweeks;
   }),
